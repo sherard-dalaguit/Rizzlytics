26b19811b00d723ae2cfce14aaf1aba1f051c910
sherard-dalaguit <sherard.softwaredev@gmail.com>
2026-01-07 15:54:07 -0800

    implemented basic photo upload to vercel blob & media-asset creation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL('https://KkqVFlzUBRB188NO.public.blob.vercel-storage.com/**'),
+    ],
+  },
 };
 
 export default nextConfig;
